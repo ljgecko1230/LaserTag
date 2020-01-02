96a03657fd3667ad355c894a3b1ca55e8935193c
Lucas James <ljgecko1230@gmail.com>
2020-01-01 17:28:31 -0800

    Client program can connect as well as configure server URL and codename

--- client.py
+++ client.py
@@ -1,13 +1,20 @@
 import requests
+import sys
 
 server_url = "http://localhost:5000/"
 codename = "Luke"
-
 id = None
+current_stats = None
 
 def connect():
+    print("Trying to connect...")
+
     player_data = str({"codename": codename})
-    return eval(requests.post(server_url + "connect", data=player_data).text)
+    return_data = eval(requests.post(server_url + "connect", data=player_data).text)
+
+    print("Connected.")
+
+    return return_data
 
 def send_attack():
     pass
@@ -16,23 +23,63 @@ def recieve_attack():
     pass
 
 def get_player_stats():
-    return eval(requests.get(server_url + "stats/" + str(id)).text)
+    print("Updating player information...")
+
+    return_data = eval(requests.get(server_url + "stats/" + str(id)).text)
+
+    print("Done.")
+
+    return return_data
 
 print()
-print("Trying to connect...")
+while True:
+    print()
+    print("SERVER URL: " + server_url)
+    print("CODENAME: " + codename)
+    if id:
+        print("CLIENT ID: " + str(id))
+        print("HEALTH: " + str(current_stats["health"]))
+        print("SCORE: " + str(current_stats["score"]))
+        print("SHOTS: " + str(current_stats["shots"]))
+        print("HITS (TAKEN): " + str(current_stats["hits_taken"]))
+        print("HITS (GIVEN): " + str(current_stats["hits_given"]))
+        print("1. Shoot")
+        print("2. Take hit")
+        print("3. Disconnect")
+        choice = input("Choice: ")
 
-response = connect()
-id = response["id"]
+        if choice == "1":
+            print("pew. choice not available at this time.")
 
-print("Server response: " + str(response))
-print("Connected.")
+        elif choice == "2":
+            print("ouch. choice not available at this time.")
 
-print()
-print("Updating player information...")
+        elif choice == "3":
+            print("Disconnecting... choice not available at this time.")
 
-response = get_player_stats()
+        else:
+            print("That is not a valid choice. Please select again.")
 
-print("Server response: " + str(response))
+    else:
+        print("1. Connect to server")
+        print("2. Configure settings")
+        choice = input("Choice: ")
+
+        if choice == "1":
+            id = connect()["id"]
+            current_stats = get_player_stats()
+
+        elif choice == "2":
+            new_server_url = input("New server URL (blank to leave unchanged): ")
+            if new_server_url:
+                server_url = new_server_url
+
+            new_codename = input("New codename (black to leave unchanged): ")
+            if new_codename:
+                codename = new_codename
+
+        else:
+            print("That is not a valid choice. Please select again.")
 
 while True:
     pass
